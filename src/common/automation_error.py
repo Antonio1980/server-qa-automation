8e4178629a80901ae8a355a2b7133eae8e879877
antonio1980 <a220580>
2019-08-05 18:40:29 +0300

    added protection (into regression packeg level) from running on prod

--- src/common/automation_error.py
+++ src/common/automation_error.py
@@ -1,16 +1,10 @@
-from src.common import logger
-from src.common.log_decorator import automation_logger
-
-
 class AutomationError(Exception):
     
     def __init__(self, *args):
         super(Exception, self).__init__(*args)
 
-    @automation_logger(logger)
     def __str__(self):
         return "Automation error is occurred: {0}".format(self.args)
 
-    @automation_logger(logger)
     def __repr__(self):
         return "Automation error is occurred: {0}".format(self.__str__())
--- tests/conftest.py
+++ tests/conftest.py
@@ -66,6 +66,29 @@ def env():
         return "stg"
 
 
+def pytest_report_header(config):
+    if config.getoption("verbose") > 0:
+        return [f"Environment is {env}", "Let's go..."]
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+
+    outcome = yield
+    rep = outcome.get_result()
+
+    if rep.when == "call" and rep.failed:
+        mode = "a" if os.path.exists("failures") else "w"
+        with open("failures", mode) as f:
+
+            if "tmpdir" in item.fixturenames:
+                extra = " (%s)" % item.funcargs["tmpdir"]
+            else:
+                extra = ""
+
+            f.write(rep.nodeid + extra + "\n")
+
+
 # def pytest_addoption(parser):
 #     parser.addoption(
 #         "--expect_endpoints", action="store", default="2", help="Please, set an expected endpoints (int).")
