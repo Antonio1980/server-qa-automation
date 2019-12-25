6b896df377e6273998aba08e688aca97c2303ade
antonio1980 <antons@eyenet-mobile.com>
2019-12-25 18:37:34 +0200

    multithread test now run with 50 threads = 20 sec.

--- tests/regression_tests/log_fetch_service_tests/delete_1000_user_messages_test.py
+++ tests/regression_tests/log_fetch_service_tests/delete_1000_user_messages_test.py
@@ -8,11 +8,12 @@ from src.base.utils.auth_zero import AuthorizationZero
 from src.base.utils.log_decorator import automation_logger
 
 test_case = "DELETE USER TASKS"
+user_id = "server-qa-automation"
 num_threads = 50
 num_loops = 20
 
 _api_client = ApiClient(AuthorizationZero.get_authorization_token()["access_token"])
-_api_client.log_fetch_svc.delete_user_tasks("server-qa-automation")
+_api_client.log_fetch_svc.delete_user_tasks(user_id)
 
 
 @pytest.mark.incremental
@@ -23,7 +24,7 @@ def test_add_1000_user_messages():
     allure.step(f"Verify that service able to accept {(num_threads * num_loops) + num_loops} messages.")
 
     for _ in range(num_loops):
-        _api_client.log_fetch_svc.add_task("server-qa-automation")
+        _api_client.log_fetch_svc.add_task(user_id)
 
     logger.logger.info(F"============ TEST CASE {test_case} / 1 PASSED ===========")
 
@@ -34,7 +35,7 @@ def test_add_1000_user_messages():
 def test_delete_1000_user_messages():
     allure.step(f"Verify that service able to delete {(num_threads * num_loops) + num_loops} messages.")
     time.sleep(5.0)
-    _response = _api_client.log_fetch_svc.delete_user_tasks("server-qa-automation")[0]
+    _response = _api_client.log_fetch_svc.delete_user_tasks(user_id)[0]
 
     assert _response["deletedCount"] == 1020
 
